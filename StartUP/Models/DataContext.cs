eee56b6dd2cc2192bc4297db3f4f7efbf86c1c0e
phucnam2610 <namkhangasd2003@gmail.com>
2023-10-12 08:51:41 +0700

    Add project files.

---
+++ StartUP/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace StartUP.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Menu> Menus { get; set; }
+    }
+}
